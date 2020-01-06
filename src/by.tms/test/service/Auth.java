66ec5d5f0bbc8b3a126bfa5ec793ab0a4b5bbd74
bystrov92@gmail.com <sergei7585609>
2020-01-06 20:00:00 +0300

    testo

--- src/by.tms/test/service/Auth.java
+++ src/by.tms/test/service/Auth.java
@@ -6,11 +6,11 @@ import by.tms.test.util.ReaderImpl;
 
 public class Auth {
     ReaderImpl reader = new ReaderImpl();
+    /*tesl lmao*/
+//    public User doAuth(){
+//        System.out.println("Enter Login");
+//        String login = reader.input();
 
-    public User doAuth(){
-        System.out.println("Enter Login");
-        String login = reader.input();
-
-        return ;
-    }
+//        return ;
+//    }
 }
